f0107134b202ca6d822f779993805c0b485c5a77
RubLAA <deaguileta@gmail.com>
2025-05-03 18:16:12 +0200

    más codigo en ui

--- gestor/ui.py
+++ gestor/ui.py
@@ -1,7 +1,8 @@
-from tkinter import Tk, Button
+from tkinter import Tk, Frame, CENTER, NO, Scrollbar, RIGHT, Y
+from tkinter import ttk
 
 class CenterWidgetMixin: 
-    def center(self,): 
+    def center(self):  # Quita la coma extra
         self.update() 
         w = self.winfo_width() 
         h = self.winfo_height() 
@@ -10,21 +11,46 @@ class CenterWidgetMixin:
         x = int((ws/2) - (w/2)) 
         y = int((hs/2) - (h/2)) 
         self.geometry(f"{w}x{h}+{x}+{y}")
-        
+
 class MainWindow(Tk, CenterWidgetMixin):
     def __init__(self): 
         super().__init__() 
         self.title('Gestor de clientes') 
         self.build() 
-        self.center() # new
+        self.center()
 
-    def build(self):
-        # Usa "self" como contenedor del botón (no self.root)
-        button = Button(self, text="Hola", command=self.hola)
-        button.pack()
+    def build(self): 
+        # Top Frame 
+        frame = Frame(self) 
+        frame.pack() 
+        
+        # Treeview 
+        treeview = ttk.Treeview(frame) 
+        treeview['columns'] = ('DNI', 'Nombre', 'Apellido') 
+        
+        # Column format 
+        treeview.column("#0", width=0, stretch=NO) 
+        treeview.column("DNI", anchor=CENTER) 
+        treeview.column("Nombre", anchor=CENTER) 
+        treeview.column("Apellido", anchor=CENTER) 
+        
+        # Heading format 
+        treeview.heading("#0", anchor=CENTER) 
+        treeview.heading("DNI", text="DNI", anchor=CENTER) 
+        treeview.heading("Nombre", text="Nombre", anchor=CENTER) 
+        treeview.heading("Apellido", text="Apellido", anchor=CENTER) 
+        
+        # Pack (solo una vez)
+        treeview.pack()
 
-    def hola(self):
-        print("¡Hola mundo!")
+        # Scrollbar 
+        scrollbar = Scrollbar(frame) # new 
+        scrollbar.pack(side=RIGHT, fill=Y) # new 
+        
+        # Treeview 
+        treeview = ttk.Treeview(frame, yscrollcommand=scrollbar.set) # edited 
+        treeview['columns'] = ('DNI', 'Nombre', 'Apellido') 
+        treeview.pack() # edited
 
 if __name__ == "__main__":
     app = MainWindow()
