d24aa37d633af845e218468443d934b9333a6152
RubLAA <deaguileta@gmail.com>
2025-05-03 19:55:36 +0200

    run final

--- gestor/run.py
+++ gestor/run.py
@@ -1,4 +1,12 @@
+import sys 
 import menu 
+import ui 
  
 if __name__ == "__main__": 
-    menu.iniciar() 
+    # Si pasamos un argumento -t lanzamos el modo terminal 
+    if len(sys.argv) > 1 and sys.argv[1] == "-t": 
+        menu.iniciar() 
+    # En cualquier otro caso lanzamos el modo gráfico 
+    else: 
+        app = ui.MainWindow() 
+        app.mainloop()
